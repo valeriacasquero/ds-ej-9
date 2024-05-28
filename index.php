2b564982a6b4355362ab51c3595b53112545ae87
valeriacasquero <valeriacasquero@gmail.com>
2024-05-27 23:28:13 -0300

    unico-commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$dias_Semana = array ("Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo");
+
+foreach ($dias_Semana as $posicion => $i) {
+    echo $i.' : '.$posicion. '<br>';
+}
+
+?>
